8262d4a4e9ffdfd0b80d2a689faada511bef926c
RustyNova <rusty.nova.jsb@gmail.com>
2026-09-03 10:53:37 +0200

    chore: unpin berger in flake (pin handled by flake.lock)

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    berger.url = "github:RustyNova016/cargo-berger/863b9d007750b6ab9f70e4fdbe2d98ed59d8dc79";
+    berger.url = "github:RustyNova016/cargo-berger";
   };
 
   outputs =
